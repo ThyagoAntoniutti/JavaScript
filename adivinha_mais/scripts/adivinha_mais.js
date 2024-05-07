6c5b58785d895d8bf835154739b1a3623f0829de
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-07 13:39:32 -0300

    update

---
+++ adivinha_mais/scripts/adivinha_mais.js
@@ -0,0 +1,24 @@
+function pulaLinha() {
+    document.write("<br><br>");
+}
+
+function mostra(frase) {
+    document.write(frase);
+    pulaLinha();
+}
+
+function verificar() {
+    if (input.value == segredo) {
+        alert("Você acertou!");
+    }else{
+        alert("Você errou!");
+    }
+}
+
+mostra("Digite um número entre 1 e 10");
+
+var segredo = Math.round(Math.random() * 10);
+var input = document.querySelector("input");
+var botao = document.querySelector("button");
+
+botao.onclick = verificar;
