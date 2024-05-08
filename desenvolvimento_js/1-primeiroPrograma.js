514e1f2c907d86d0bc53fe44043b998add0d55ac
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-08 10:51:15 -0300

    update

---
+++ desenvolvimento_js/1-primeiroPrograma.js
@@ -0,0 +1,15 @@
+console.log("Meu primeiro programa! Trabalhando com Variáveis");
+
+const idade = 26;
+
+console.log(idade);
+
+console.log("Soma: " + idade + 2);
+console.log("Subtração: " + idade - 2);
+console.log("Multiplicação: " + idade * 2);
+console.log("Divisão: " + idade / 2);
+console.log("Resto da Divisão: " + idade % 2);
+
+
+const idadeSomada = idade + 2;
+console.log("A idade somada é: " + idadeSomada);
