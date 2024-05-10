8eed57f43d961b690af5f4a1a445924df3a28af5
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-10 16:33:52 -0300

    update

--- desenvolvimento_js/8-loops.js
+++ desenvolvimento_js/8-loops.js
@@ -23,6 +23,12 @@ const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 let contador = 0;
 
 while (contador < length.listaDeDestinos) {
-    console.log(listaDeDestinos[contador]);
+    if (listaDeDestinos[contador] == destino){
+        console.log("O destino ESTÁ disponível.");
+    } else {
+        console.log("O destino NÃO ESTÁ disponível.");
+    }
+
     contador++;
+
 }
