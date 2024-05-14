297c3df1e3237057160447f90fc382aa0bbb1fe7
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-14 14:05:17 -0300

    update

--- desenvolvimento_js/8-loops.js
+++ desenvolvimento_js/8-loops.js
@@ -23,25 +23,35 @@ const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 let contador = 0;
 let destinoExiste = false;
 
-while (contador < 7) {
+// while (contador < 7) {
 
+//     if (listaDeDestinos[contador] == destino){
+//         console.log("O destino ESTÁ disponível.");
+//         destinoExiste = true;
+//         break;
+//     } else {
+//         destinoExiste = false;
+//         console.log("O destino NÃO ESTÁ disponível.");
+//     }
+
+//     contador++;
+
+// }
+
+// console.log("O destino existe:", destinoExiste);
+
+// if (podeComprar && destinoExiste) {
+//     console.log("Tenha uma boa viagem!");
+// } else {
+//     console.log("Houston, temos um problema!");3
+// }
+
+for (let cont = 0; cont < 7; cont++){
     if (listaDeDestinos[contador] == destino){
         console.log("O destino ESTÁ disponível.");
         destinoExiste = true;
         break;
     } else {
-        destinoExiste = false;
         console.log("O destino NÃO ESTÁ disponível.");
     }
-
-    contador++;
-
-}
-
-console.log("O destino existe:", destinoExiste);
-
-if (podeComprar && destinoExiste) {
-    console.log("Tenha uma boa viagem!");
-} else {
-    console.log("Houston, temos um problema!");3
 }
