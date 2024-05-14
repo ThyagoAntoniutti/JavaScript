4c9a76ac83e6c04d72dd874baea59422044d3ada
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-14 17:10:24 -0300

    update

--- desenvolvimento_js/banco/contaCorrente.js
+++ desenvolvimento_js/banco/contaCorrente.js
@@ -1,9 +1,3 @@
-class Cliente {
-    nome;
-    cpf;
-    rg;
-}
-
 class ContaCorrente {
     agencia;
     conta;
@@ -33,31 +27,3 @@ class ContaCorrente {
         }
     }
 }
-
-const cliente1 = new Cliente();
-
-cliente1.nome = "Guilherme";
-cliente1.cpf = 11122233309;
-cliente1.rg = 123456789;
-
-const cliente2 = new Cliente();
-
-cliente2.nome = "André";
-cliente2.cpf = 44455566609;
-cliente2.rg = 456789123;
-
-console.log(cliente1);
-console.log(cliente2);
-
-const contaCorrenteGuilherme = new ContaCorrente();
-
-contaCorrenteGuilherme.agencia = 422;
-contaCorrenteGuilherme.conta = 24653;
-contaCorrenteGuilherme.digito = 7;
-
-contaCorrenteGuilherme.depositar(300);
-const valorSacado = contaCorrenteGuilherme.sacar(50);
-
-console.log("Você sacou: " + valorSacado + " reais.");
-
-console.log(contaCorrenteGuilherme);
