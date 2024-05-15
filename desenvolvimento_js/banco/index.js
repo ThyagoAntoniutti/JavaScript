a7e49e2502110d2f572a93c4486bf5afee953e33
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-15 10:58:33 -0300

    update

--- desenvolvimento_js/banco/index.js
+++ desenvolvimento_js/banco/index.js
@@ -7,14 +7,14 @@ cliente1.nome = "Guilherme";
 cliente1.cpf = 11122233309;
 cliente1.rg = 123456789;
 
-const cliente2 = new Cliente();
+// const cliente2 = new Cliente();
 
-cliente2.nome = "André";
-cliente2.cpf = 44455566609;
-cliente2.rg = 456789123;
+// cliente2.nome = "André";
+// cliente2.cpf = 44455566609;
+// cliente2.rg = 456789123;
 
 console.log(cliente1);
-console.log(cliente2);
+// console.log(cliente2);
 
 const contaCorrenteGuilherme = new ContaCorrente();
 contaCorrenteGuilherme.cliente = cliente1;
@@ -24,22 +24,28 @@ contaCorrenteGuilherme.conta = 24653;
 contaCorrenteGuilherme.digito = 7;
 
 const contaCorrenteAndre = new ContaCorrente();
-contaCorrenteAndre.cliente = cliente2;
+contaCorrenteAndre.cliente = new Cliente();
+contaCorrenteAndre.cliente.nome = "André";
+contaCorrenteAndre.cliente.cpf = 44455566609;
+contaCorrenteAndre.cliente.rg = 456789123;
+
 
 contaCorrenteAndre.agencia = 422;
 contaCorrenteAndre.conta = 84652;
 contaCorrenteAndre.digito = 3;
 
-console.log(contaCorrenteGuilherme);
 console.log(contaCorrenteAndre);
 
-contaCorrenteGuilherme.depositar(500);
+// console.log(contaCorrenteGuilherme);
+// console.log(contaCorrenteAndre);
 
-console.log(contaCorrenteGuilherme);
+// contaCorrenteGuilherme.depositar(500);
 
-let valorTransferido = 200;
-contaCorrenteGuilherme.transferir(valorTransferido, contaCorrenteAndre);
-console.log("Valor Transferido: " + valorTransferido)
+// console.log(contaCorrenteGuilherme);
 
-console.log(contaCorrenteAndre);
-console.log(contaCorrenteGuilherme);
+// let valorTransferido = 200;
+// contaCorrenteGuilherme.transferir(valorTransferido, contaCorrenteAndre);
+// console.log("Valor Transferido: " + valorTransferido)
+
+// console.log(contaCorrenteAndre);
+// console.log(contaCorrenteGuilherme);
