629564ad2a10b06200261b46b49fa6fca65171dd
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-15 17:52:53 -0300

    update

---
+++ cfb/aula08/aula08.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log("Trabalhando com operadores Bitwise em JavaScript");
+
+let n1 = 10;
+let n2 = 11;
+
+let res = n1 & n2;
+
+console.log(res);
---
+++ cfb/aula09/aula09.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log("Entendendo pré incremento e pós incremento");
+
+let n = 10;
+
+console.log(n);
