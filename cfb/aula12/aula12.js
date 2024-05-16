6be9e90543eeab9600b6ef728e925329f07eb160
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-16 16:10:27 -0300

    update

---
+++ cfb/aula12/aula12.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log("Entendendo o opreador spread");
+// basicamente o spread serve para quebrarmos um array
+// sprad - espalhar
+// ele quebra um conjunto de elementos e devolve elemento a elemento
+
+let n1 = [10, 20, 30];
+let n2 = [11, 22, 33, 44, 55];
+let n3
+
+console.log("n1: " + n1);
+console.log("n2: " + n2);
+console.log("n3: " + n3);
