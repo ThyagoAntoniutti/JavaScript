819770d983a36ff1f6ade53e7ef526195347a626
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-16 14:48:47 -0300

    update

--- cfb/aula09/aula09.js
+++ cfb/aula09/aula09.js
@@ -2,6 +2,23 @@
 
 console.log("Entendendo pré incremento e pós incremento");
 
-let n = 10;
+// o - neste caso é um operador de inversão, invertendo o valor
+// ou seja, se está negativo se torna positivo e vice versa
+let n = -10;
 
-console.log(n);
+// quando a operação está a direita da variavel, ela é uma 
+// operação chamada de pós incremento
+// ou seja, primeiro usamos o valor e depois incrementamos (vale para incremento e decremento)
+// n++;
+
+// quando incrementamos desta forma, primeiro ele vai imprimir
+// o valor da forma que foi declarado, pois está primeiro pegando
+// o valor e DEPOIS incrementando. PÓS INCREMENTO, usa depois incrementa
+console.log(n++);
+
+// já neste caso, temos a operação de PRÉ INCREMENTO, em que PRIMEIRO
+// incrementamos e DEPOIS usamos o valor já com o incremento
+console.log(++n);
+
+// neste caso ele opera a variável, ele vai realmente opera-la, seja 
+// como pré incremento ou pós incremento
