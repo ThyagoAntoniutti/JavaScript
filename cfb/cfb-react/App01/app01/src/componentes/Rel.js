8053506025bec789d9d409c530f5045b87f4d80f
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-19 18:24:30 -0300

    update

--- cfb/cfb-react/App01/app01/src/componentes/Rel.js
+++ cfb/cfb-react/App01/app01/src/componentes/Rel.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Relogio() {
     return (
-        <p>
+        <p className="relogio">
             {new Date().toLocaleTimeString()}
         </p>
     )
