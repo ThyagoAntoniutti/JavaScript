f628fc47391db00ac27b526678d5240187a44cc4
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-20 15:58:32 -0300

    update

--- cfb/cfb-react/App03/app03/src/componentes/Exercicio02.js
+++ cfb/cfb-react/App03/app03/src/componentes/Exercicio02.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Exercicio02() {
+
+    const [nota1, setNota1] = useState();
+    const [nota2, setNota2] = useState();
+    const [nota3, setNota3] = useState();
+    const [nota4, setNota4] = useState();
+
+    let soma = parseInt(nota1) + parseInt(nota2) + parseInt(nota3) + parseInt(nota4)
+    let media = soma / 4;
+    console.log(media);
+
     return (
+
         <>
             <section className="container">
                 <header>
@@ -13,24 +24,52 @@ export default function Exercicio02() {
 
                     <section className="containerNotas">
                         <section className="nota">
-                            <label>Nota 1:</label><input type="number" className="textoNota"/>
+                            <label>Nota 1:</label>
+                            <input 
+                            type="number" 
+                            name="nota1"
+                            value={nota1}
+                            onChange={(e) => setNota1(e.target.value)}
+                            className="textoNota"/>
                         </section>
 
                         <section className="nota">
-                            <label>Nota 2:</label><input type="number" className="textoNota"/>
+                            <label>Nota 2:</label>
+                            <input 
+                            type="number" 
+                            name="nota2"
+                            value={nota2}
+                            onChange={(e) => setNota2(e.target.value)}
+                            className="textoNota"/>
                         </section>
 
                         <section className="nota">
-                            <label>Nota 3:</label><input type="number" className="textoNota"/>
+                            <label>Nota 3:</label>
+                            <input 
+                            type="number"
+                            name="nota3"
+                            value={nota3}
+                            onChange={(e) => setNota3(e.target.value)} 
+                            className="textoNota"/>
                         </section>
 
                         <section className="nota">
-                            <label>Nota 4:</label><input type="number" className="textoNota"/>
+                            <label>Nota 4:</label>
+                            <input 
+                            type="number" 
+                            name="nota4"
+                            value={nota4}
+                            onChange={(e) => setNota4(e.target.value)}
+                            className="textoNota"/>
                         </section>
                     </section>
 
                     <section>
                         <button className="botaoCalcular">Calcular Média</button>
+                    </section>
+
+                    <section>
+                        <p>{media}</p>
                     </section>
                     
                 </main>
