ca0be617a616a7f2de51eff7ca092bf970dcdd53
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-23 20:02:56 -0300

    update

---
+++ cfb/cfb-javascript/aula15/aula15.js
@@ -0,0 +1,24 @@
+"use strict"
+
+console.log("Trabalhando com loops de repetição - LOOP FOR");
+
+// Basicamente uma estrutura de loop é uma estrutura em que precisamos
+// repetir um determinado conjunto de funçoes em um determinado ou indeterminado
+// número de vezes. As repetições são chamadas ITERAÇÕES.
+
+// Os loops podem ser classificados em definidos e indefinidos. Os definidos são os 
+// que sabemos quantas vezes precisamos repetir / iterar. Já os indefinidos são os que
+// não sabemos ao certo quantas vezes iremos repetir / iterar quele bloco de código.
+
+// DEFINIDOS --> FOR -----> Usado quando sabemos quantas vezes precisamos repetir / iterar
+// INDEFINIDOS --> WHILE --> DO WHILE -----> Usado quando NÃO sabemos quantas vezes precisamos repetir / iterar
+
+// ESTRUTURA DO FOR
+// for (inicialização da variável do contador ; condição (o que vai avaliar se continuamos iterando ou não; controle (++ ou -- da variável do contador))){
+//    comandos que serão 
+//    iterados pelo código
+//}
+
+for (let i = 0; i < 10; i++) {
+    console.log("Entendendo o funcionamento do FOR");
+}
