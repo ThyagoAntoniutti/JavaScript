5daae1fe1ef9e82dd73721f7cab2a2cc3e8eab44
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-23 18:56:41 -0300

    update

---
+++ cfb/cfb-javascript/aula13/aula13.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log("Trabalhando com comandos condicionais IF e ELSE");
+
+let num = 10;
+
+if (num > 10) {
+    console.log("num é maior que 10");
+} 
+
+console.log("fim do programa");
