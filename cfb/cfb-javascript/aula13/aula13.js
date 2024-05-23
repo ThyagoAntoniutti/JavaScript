6c3e89d9643708ff11e9a33b895cd5807e3fc4ef
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-23 18:57:28 -0300

    update

--- cfb/cfb-javascript/aula13/aula13.js
+++ cfb/cfb-javascript/aula13/aula13.js
@@ -4,7 +4,8 @@ console.log("Trabalhando com comandos condicionais IF e ELSE");
 
 let num = 10;
 
-if (num > 10) {
+// Quando o IF possui apenas um comando em seu interior não é necessáio colocarmos as chaves
+if (num <= 10) {
     console.log("num é maior que 10");
 } 
 
