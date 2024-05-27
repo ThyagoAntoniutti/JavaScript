e88f9f7e422b96036710a307ece25b1393107acc
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-27 15:20:36 -0300

    update

--- cfb/cfb-javascript/aula20/aula20.js
+++ cfb/cfb-javascript/aula20/aula20.js
@@ -3,9 +3,14 @@
 console.log("Trabalhando com FUNÇÕES que RETORNAM valors");
 
 function trampo() {
-    console.log("Desenvolvimento de Sistemas")
+    return "Thyago Capeleti";
+    // Pare que retorne alguma coisa precisamos utilizar o return. A função vai 
+    // realizar todo o processamento e então vai retornar algo para quem fez a chamada da mesma
 }
 
-trampo();
-trampo();
-trampo();
+// Se apenas chamarmos a função, não será impresso nada na tela, então podemos usar o console.log
+// para que vejamos o que foi retornado.
+console.log(trampo());
+console.log(trampo());
+console.log(trampo());
+
