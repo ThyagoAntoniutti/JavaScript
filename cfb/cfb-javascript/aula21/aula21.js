a1a0ba05c6a5d21ce2b3849fd2267790d1c373b5
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-27 15:38:06 -0300

    update

--- cfb/cfb-javascript/aula21/aula21.js
+++ cfb/cfb-javascript/aula21/aula21.js
@@ -3,10 +3,19 @@
 console.log("Trabalhando com FUNÇÕES PARAMETRIZADAS");
 
 // Funções parametrizadas é quando podemos entrar com valores para dentro de nossa função
+// Podemos passar quantos parâmetros nós precisarmos
+
+function exemplo(parametro1) {
+    console.log(parametro1);
+}
 
 function soma(n1, n2) {
     let soma = n1 + n2;
     console.log(soma);
 }
+
+exemplo("Thyago");
+exemplo("Capeleti");
+exemplo("Sistemas de Informação");
 
 soma(10, 15);
