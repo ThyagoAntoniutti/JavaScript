ed5a22d46d050ecea404ad7794a9d798975a34f7
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-29 23:19:41 -0300

    update

---
+++ cfb/cfb-javascript/aula24/aula24.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log("Trabalhando com FUNÇÕES ANÔNIMAS");
+
+// Basicamente são funções que não possuem nomes relacionados ao seu corpo, seu conteúdo
+// São funções chamadas em tempo de execução. Ou seja, quando criamos uma função normal, ela só é executada quando chamada
+// Já as funções anônimas não são estanciadas, não estão na memória. Elas só são criadas no momento de sua execução
+// Ela funciona como uma funçao padrão, pode ser parametrizada, etc... 
+// Continuar com a parte de desenvolvimento em React também 
