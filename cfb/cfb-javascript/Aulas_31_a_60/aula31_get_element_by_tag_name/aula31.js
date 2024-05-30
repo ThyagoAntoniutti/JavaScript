2eaeaa6d2123211b46df4ab2f848f40fbc02ff96
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-30 20:59:40 -0300

    update

--- cfb/cfb-javascript/Aulas_31_a_60/aula31_get_element_by_tag_name/aula31.js
+++ cfb/cfb-javascript/Aulas_31_a_60/aula31_get_element_by_tag_name/aula31.js
@@ -10,7 +10,11 @@ const c6 = document.getElementById('c6');
 const arrayElementos = [c1, c2, c3, c4, c5, c6];
 
 // Tag name retorna uma coleção de elementos
-const colecaoHTML = document.getElementsByTagName('div');
+// Podemos puxar usando spread para espalhar os elementos. Dessa forma, não temos mais um HTML COLLECTION mas sim um ARRAY
+// Aqui temos a vantagem de estarmos usando uma constante e não corrermos o risco de alterar estes elementos
+const colecaoHTML = [...document.getElementsByTagName('div')];
+// mesma coisa que
+// colecaoHTML = [...colecaoHTML];
 
 console.log(colecaoHTML);
 
