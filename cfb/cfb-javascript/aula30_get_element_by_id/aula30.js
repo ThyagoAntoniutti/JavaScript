0e8e12d3d1f7761eb6e50b931b3f821e0ed5ace9
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-30 20:26:22 -0300

    update

---
+++ cfb/cfb-javascript/aula30_get_element_by_id/aula30.js
@@ -0,0 +1,14 @@
+// get element by id - usado para obtermos um elemento específico através do ID
+
+const c1 = document.getElementById('c1');
+const c2 = document.getElementById('c2');
+const c3 = document.getElementById('c3');
+const c4 = document.getElementById('c4');
+const c5 = document.getElementById('c5');
+const c6 = document.getElementById('c6');
+
+
+console.log(c1);
+console.log(c1.id);
+console.log(c1.innerHTML);
+c1.innerHTML = 'TROCADO';
