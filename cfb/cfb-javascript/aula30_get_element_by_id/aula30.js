8cdf75aa2b6d5f13046a0677ff498dc9542225b8
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-30 20:31:31 -0300

    update

--- cfb/cfb-javascript/aula30_get_element_by_id/aula30.js
+++ cfb/cfb-javascript/aula30_get_element_by_id/aula30.js
@@ -7,8 +7,21 @@ const c4 = document.getElementById('c4');
 const c5 = document.getElementById('c5');
 const c6 = document.getElementById('c6');
 
+const arrayElementos = [c1, c2, c3, c4, c5, c6];
 
-console.log(c1);
-console.log(c1.id);
-console.log(c1.innerHTML);
-c1.innerHTML = 'TROCADO';
+// console.log(c1);
+// console.log(c1.id);
+// console.log(c1.innerHTML);
+// c1.innerHTML = 'TROCADO';
+// console.log(arrayElementos);
+
+// Podemos associar esses elementos a um array para alterar todos
+// for (e of arrayElementos) {
+//     e.innerHTML = 'TROCADO';
+// }
+
+// Também podemos utilizar o MAP para operar essas variáveis
+arrayElementos.map((e) => {
+    e.innerHTML = 'TROCADO';
+    console.log(e);
+})
