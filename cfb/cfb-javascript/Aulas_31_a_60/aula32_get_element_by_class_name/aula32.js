cf9b438c02ce3428ab75463fd5c6c943edb506a7
Thyago <dev.thyago.capeleti@outlook.com>
2024-05-30 21:18:33 -0300

    update

--- cfb/cfb-javascript/Aulas_31_a_60/aula32_get_element_by_class_name/aula32.js
+++ cfb/cfb-javascript/Aulas_31_a_60/aula32_get_element_by_class_name/aula32.js
@@ -1,26 +1,5 @@
-console.log('Trabalhando com getElementByTagName');
+console.log('Trabalhando com tagElementByClassName')
 
-const c1 = document.getElementById('c1');
-const c2 = document.getElementById('c2');
-const c3 = document.getElementById('c3');
-const c4 = document.getElementById('c4');
-const c5 = document.getElementById('c5');
-const c6 = document.getElementById('c6');
+const cursosTodos = [...document.getElementsByClassName('curso')];
 
-const arrayElementos = [c1, c2, c3, c4, c5, c6];
-
-// Tag name retorna uma coleção de elementos
-// Podemos puxar usando spread para espalhar os elementos. Dessa forma, não temos mais um HTML COLLECTION mas sim um ARRAY
-// Aqui temos a vantagem de estarmos usando uma constante e não corrermos o risco de alterar estes elementos
-const colecaoHTML = [...document.getElementsByTagName('div')];
-// mesma coisa que
-// colecaoHTML = [...colecaoHTML];
-
-console.log(colecaoHTML);
-
-colecaoHTML.map ((e) => {
-    console.log(e);
-})
-
-// console.log(arrayElementos);
-// console.log(colecaoHTML);
+console.log(cursosTodos);
