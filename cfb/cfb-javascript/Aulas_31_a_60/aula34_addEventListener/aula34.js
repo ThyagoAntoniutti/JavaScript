d5f36a9b89eda0a875803a9693b4fbafc502a5ce
Thyago <dev.thyago.capeleti@outlook.com>
2024-06-06 15:07:45 -0300

    update

--- cfb/cfb-javascript/Aulas_31_a_60/aula34_addEventListener/aula34.js
+++ cfb/cfb-javascript/Aulas_31_a_60/aula34_addEventListener/aula34.js
@@ -8,22 +8,39 @@ console.log("Trabalhando com eventos");
 // Podemos chamar uma função em cada um desses elementos para funcionar
 
 
-function msg() {
-    alert('Clicou com JS externo,');
-}
+// function msg() {
+//     alert('Clicou com JS externo,');
+// }
 
-const msg_arrow = () => {
-    alert('Clicou com Arrow Function,')
-}
+// const msg_arrow = () => {
+//     alert('Clicou com Arrow Function,')
+// }
 
 // const c4 = document.getElementById('c4');
-const c4 = document.querySelector('#c4');
-
-const msg_evento = () => {
-    alert('Clicou usando um Evento,')
-}
+// const c4 = document.querySelector('#c4');
+// const c5 = document.querySelector('#c5');
+const cursos = [...document.querySelectorAll('.curso')];
+// const msg_evento = () => {
+//     alert('Clicou usando um Evento.')
+// }
 
 // Para adicionar os eventos usamos o addEventListener, que adiciona um Listener, um "escutador", usado para tratar elementos que estamos escutando.
 // O addEventListener precisa receber em seus parâmetros o que ele vai fazer e depois o que será executado quando este evento ocorrer. 
 // Então colocamos (evento trabalhado, o que será executado).
-c4.addEventListener('click', msg_evento);
+// c4.addEventListener('click', () => {
+//     alert('Clicou usando arrow function entro do evento.');
+// });
+
+// Um ponto MUITO IMPORTANTE. Ao executar um evento de um determinado elemento podemos capturar o evento que foi ocorrido, e nesse evento podemos saber quem disparou esse evento.
+
+cursos.map ((el) => {
+    el.addEventListener('click', (evt) => {
+        console.log(el.innerHTML + ' foi clicado')
+    })
+})
+
+// c5.addEventListener('click', (evt) => {
+//     // No console, em target, ele traz o elemento que acionou o evento.
+//     const el = evt.target;
+//     el.classList.add('Destaque');
+// });
