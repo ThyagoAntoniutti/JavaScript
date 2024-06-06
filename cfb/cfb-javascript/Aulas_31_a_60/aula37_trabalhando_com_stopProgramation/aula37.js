611320b447ae3021892a0cd2cac290a2d100d952
Thyago <dev.thyago.capeleti@outlook.com>
2024-06-06 16:23:19 -0300

    update

--- cfb/cfb-javascript/Aulas_31_a_60/aula37_trabalhando_com_stopProgramation/aula37.js
+++ cfb/cfb-javascript/Aulas_31_a_60/aula37_trabalhando_com_stopProgramation/aula37.js
@@ -1,29 +1,6 @@
-// Primeiro precisamos obter os elementos que iremos trabalhar
-
 const caixa1 = document.getElementById('caixa1');
-const caixa2 = document.getElementById('caixa2');
-const btn = document.getElementById('btnTransferir');
-const todosCursos = [...document.querySelectorAll('.curso')];
+// const btn_c1 = document.querySelector('#c1');
 
-todosCursos.map((el) => {
-    el.addEventListener('click', (evt) => {
-        const curso = evt.target;
-        curso.classList.toggle('selecionado');
-        console.log(el);
-    })
+caixa1.addEventListener('click',() => {
+    console.log('clicou');
 })
-
-btn.addEventListener('click', () => {
-    const cursosSelecionados = [...document.querySelectorAll('.selecionado')]; 
-    const cursosNaoSelecionados = [...document.querySelectorAll('.curso:not(.selecionado)')];
-    console.log(cursosNaoSelecionados);
-    cursosSelecionados.map((el) => {
-        caixa2.appendChild(el);
-    })
-    cursosNaoSelecionados.map((el) => {
-        caixa1.appendChild(el);
-    })
-
-    
-})
-
