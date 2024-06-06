63d8256edabc8af4ab5caeb17a453be7b9ae5f27
Thyago <dev.thyago.capeleti@outlook.com>
2024-06-06 17:18:36 -0300

    update

--- cfb/cfb-javascript/Aulas_31_a_60/aula37_trabalhando_com_stopProgramation/aula37.js
+++ cfb/cfb-javascript/Aulas_31_a_60/aula37_trabalhando_com_stopProgramation/aula37.js
@@ -1,6 +1,15 @@
-const caixa1 = document.getElementById('caixa1');
-// const btn_c1 = document.querySelector('#c1');
+const caixa1 = document.querySelector('#caixa1');
+const btn_c1 = document.querySelector('#c1');
 
-caixa1.addEventListener('click',() => {
-    console.log('clicou');
+// O evento é propagado para todos os elementos que estão dentro do container.
+// Então todos eles terão essa função do clique. Para resolver isso usamos o stopPropagation
+caixa1.addEventListener('click',(evento) => {
+    // Dessa forma ele vai imprimir um PointerEvent, que tem uma série de propriedades, sendo uma delas o target (quem disparou o evento).
+    // Se dermos um parâmetro.target saberemos quem disparou o evento.
+    console.log(evento.target);
+    
 })
+
+// btn_c1.addEventListener('click',() => {
+//     console.log('clicou');
+// })
